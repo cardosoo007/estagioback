04156f9ef4db2dddf259020b45cc64a510d34897
Rodrigo Cardoso adm <rodrigocra@C02FFJ5PMD6M.local>
2026-05-06 14:29:19 +0100

    new endpoint

--- app.js
+++ app.js
@@ -13,13 +13,12 @@ app.get('/test', (req, res) => {
 
 app.post('/test', (req, res) => {
     res.send('Hello Wordl')
-}
+})
 
-)
+app.post('/jogador', (req, res) => {
+    res.json({ status: "created" });
 
-
-
-
+})
 
 
 app.listen(port, () => {
