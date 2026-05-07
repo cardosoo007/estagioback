56b070c1bb257ff38f840292dcd583d94c2b3cfd
Rodrigo Cardoso adm <rodrigocra@C02FFJ5PMD6M.local>
2026-05-07 11:53:22 +0100

    adiocionar endpoints

--- app.js
+++ app.js
@@ -3,22 +3,61 @@ const app = express()
 const port = 3000
 
 //endpoints
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+app.get('/classificacoes', (req, res) => {
+
+    const classificacoes = [
+        { equipa: "Benfica", pontos: 30 },
+        { equipa: "Porto", pontos: 32 },
+        { equipa: "Sporting", pontos: 27 }
+    ];
+    res.json(classificacoes);
+})
+app.get('/equipas', (req, res) => {
+
+    const equipas = [
+        {
+            equipa: "Benfica",
+            treinador: { nome: "Jose Mourinho", idade: 57 },
+            jogadores: [
+                { nome: "Pavlidis", idade: 36, posicao: "Extremo" },
+                { nome: "Rafa", idade: 31, posicao: "Avançado" }],
+        },
+
+        {
+            equipa: "Porto",
+            treinador: { nome: "Farioli", idade: 40 },
+            jogadores: [
+                { nome: "Pepe", idade: 41, posicao: "Defesa" },
+                { nome: "Kiwior", idade: 24, posicao: "Defesa" },
+                { nome: "Zaidu", idade: 27, posicao: "Defesa" }]
+        },
+        {
+            equipa: "Sporting",
+            treinador: { nome: "Rúben Amorim", idade: 39 },
+            jogadores: [
+                { nome: "Suarez", idade: 27, posicao: "Avançado" },
+                { nome: "Pedro Gonçalves", idade: 26, posicao: "Médio" },
+                { nome: "Inácio", idade: 22, posicao: "Defesa" }]
+        }
+
+    ];
+
+    res.json(equipas);
 })
 
-app.get('/test', (req, res) => {
-    res.send('Hello World!')
-})
+app.get('/marcadores', (req, res) => {
 
-app.post('/test', (req, res) => {
-    res.send('Hello Wordl')
-})
+    const marcadores = [
+        { jogador: "Zaidu", equipa: "Porto", golos: 18 },
+        { jogador: "Suarez", equipa: "Sporting", golos: 15 },
+        { jogador: "Rafa", equipa: "Benfica", golos: 12 }
+    ];
 
-app.post('/jogador', (req, res) => {
-    res.json({ status: "created" });
+    res.json(marcadores);
+});
 
-})
+
 
 
 app.listen(port, () => {
