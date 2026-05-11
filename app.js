8d8d6842f823e2fa87f9716bf3e944469e980f5f
Rodrigo USER <rodrigoc@GGHMHNHX7W-PT001740.local>
2026-05-11 17:08:34 +0100

    Adicionar funcionalidade básica de administrador

--- app.js
+++ app.js
@@ -1,129 +1,125 @@
-const express = require('express')
-const app = express()
-const port = 3000
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+const express = require("express");
+const app = express();
+const port = 3000;
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
+//endpoints
 
-//endpoints 
-
-app.get('/classificacoes', (req, res) => {
-
-    const classificacoes = [
-        { equipa: "Benfica", pontos: 30 },
-        { equipa: "Porto", pontos: 32 },
-        { equipa: "Sporting", pontos: 27 }
-    ];
-    res.json(classificacoes);
-})
-app.get('/equipas', (req, res) => {
-
-    const equipas = [
-        {
-            id: 1,
-            equipa: "Benfica",
-            treinador: { nome: "Jose Mourinho", idade: 57 },
-            jogadores: [
-                { id: 1, nome: "Pavlidis", idade: 36, posicao: "Extremo" },
-                { id: 2, nome: "Rafa", idade: 31, posicao: "Avançado" }],
-        },
-
-        {
-            id: 2,
-            equipa: "Porto",
-            treinador: { nome: "Farioli", idade: 40 },
-            jogadores: [
-                { id: 3, nome: "Pepe", idade: 41, posicao: "Defesa" },
-                { id: 4, nome: "Kiwior", idade: 24, posicao: "Defesa" },
-                { id: 5, nome: "Zaidu", idade: 27, posicao: "Defesa" }]
-        },
-        {
-            id: 3,
-            equipa: "Sporting",
-            treinador: { nome: "Rúben Amorim", idade: 39 },
-            jogadores: [
-                { id: 6, nome: "Suarez", idade: 27, posicao: "Avançado" },
-                { id: 7, nome: "Pedro Gonçalves", idade: 26, posicao: "Médio" },
-                { id: 8, nome: "Inácio", idade: 22, posicao: "Defesa" }]
-        }
-
-    ];
-
-    res.json(equipas);
-})
-
-app.get('/marcadores', (req, res) => {
-
-    const marcadores = [
-        { id: 5, jogador: "Zaidu", equipa: "Porto", golos: 18 },
-        { id: 6, jogador: "Suarez", equipa: "Sporting", golos: 15 },
-        { id: 2, jogador: "Rafa", equipa: "Benfica", golos: 12 },
-        { id: 4, jogador: "Kiwior", equipa: "Porto", golos: 10 },
-    ];
-
-    res.json(marcadores);
+app.get("/classificacoes", (req, res) => {
+  const classificacoes = [
+    { equipa: "Benfica", pontos: 30 },
+    { equipa: "Porto", pontos: 32 },
+    { equipa: "Sporting", pontos: 27 },
+  ];
+  res.json(classificacoes);
+});
+app.get("/equipas", (req, res) => {
+  res.json(equipasDB);
 });
 
-app.get('/jogadores/:idjogador', (req, res) => {
+app.get("/marcadores", (req, res) => {
+  const marcadores = [
+    { id: 5, jogador: "Zaidu", equipa: "Porto", golos: 18 },
+    { id: 6, jogador: "Suarez", equipa: "Sporting", golos: 15 },
+    { id: 2, jogador: "Rafa", equipa: "Benfica", golos: 12 },
+    { id: 4, jogador: "Kiwior", equipa: "Porto", golos: 10 },
+  ];
 
-
-    const marcadores = [
-        { id: 5, nome: "Zaidu", idade: 27, posicao: "Avançado", equipa: "Porto", golos: 18 },
-        { id: 6, nome: "Suarez", idade: 27, posicao: "Avançado", equipa: "Sporting", golos: 15 },
-        { id: 2, nome: "Rafa", idade: 31, posicao: "Avançado", equipa: "Benfica", golos: 12 },
-        { id: 4, nome: "Kiwior", idade: 24, posicao: "Defesa", equipa: "Porto", golos: 10 },
-    ];
-
-    const jogador = marcadores.find(marcador => marcador.id == (req.params.idjogador));
-    console.log(jogador);
-    res.json(jogador);
+  res.json(marcadores);
 });
 
-app.get('/equipas/:idequipa', (req, res) => {
-    const equipas = [
-        {
-            id: 1,
-            equipa: "Benfica", treinador: { nome: "Jose Mourinho", idade: 57 },
-            pontos: 30,
-            jogadores: [
-                { id: 1, nome: "Pavlidis", idade: 36, posicao: "Extremo" },
-                { id: 2, nome: "Rafa", idade: 31, posicao: "Avançado" }],
-        },
+app.get("/jogadores/:idjogador", (req, res) => {
+  const marcadores = [
+    {
+      id: 5,
+      nome: "Zaidu",
+      idade: 27,
+      posicao: "Avançado",
+      equipa: "Porto",
+      golos: 18,
+    },
+    {
+      id: 6,
+      nome: "Suarez",
+      idade: 27,
+      posicao: "Avançado",
+      equipa: "Sporting",
+      golos: 15,
+    },
+    {
+      id: 2,
+      nome: "Rafa",
+      idade: 31,
+      posicao: "Avançado",
+      equipa: "Benfica",
+      golos: 12,
+    },
+    {
+      id: 4,
+      nome: "Kiwior",
+      idade: 24,
+      posicao: "Defesa",
+      equipa: "Porto",
+      golos: 10,
+    },
+  ];
 
-        {
-            id: 2,
-            equipa: "Porto", treinador: { nome: "Farioli", idade: 40 },
-            pontos: 32,
-            jogadores: [
-                { id: 3, nome: "Pepe", idade: 41, posicao: "Defesa" },
-                { id: 4, nome: "Kiwior", idade: 24, posicao: "Defesa" },
-                { id: 5, nome: "Zaidu", idade: 27, posicao: "Defesa" }]
-        },
-        {
-            id: 3,
-            equipa: "Sporting", treinador: { nome: "Rúben Amorim", idade: 39 },
-            pontos: 27,
-            jogadores: [
-                { id: 6, nome: "Suarez", idade: 27, posicao: "Avançado" },
-                { id: 7, nome: "Pedro Gonçalves", idade: 26, posicao: "Médio" },
-                { id: 8, nome: "Inácio", idade: 22, posicao: "Defesa" }]
-        }
-
-    ];
-
-    const equipa = equipas.find(equipa => equipa.id == req.params.idequipa);
-    console.log(equipa);
-    res.json(equipa);
+  const jogador = marcadores.find(
+    (marcador) => marcador.id == req.params.idjogador,
+  );
+  console.log(jogador);
+  res.json(jogador);
 });
 
-app.post('/equipas', (request, response) => {
-    console.log(request.body);
-    response.json({});
+const equipasDB = [
+  {
+    id: 1,
+    equipa: "Benfica",
+    treinador: { nome: "Jose Mourinho", idade: 57 },
+    pontos: 30,
+  },
+
+  {
+    id: 2,
+    equipa: "Porto",
+    treinador: { nome: "Farioli", idade: 40 },
+    pontos: 32,
+  },
+  {
+    id: 3,
+    equipa: "Sporting",
+    treinador: { nome: "Rúben Amorim", idade: 39 },
+    pontos: 27,
+  },
+  {
+    id: 4,
+    equipa: "Valonguese",
+    treinador: { nome: "Joao Sousa", idade: 60 },
+    pontos: 27,
+  },
+];
+
+app.get("/equipas/:idequipa", (req, res) => {
+  const equipa = equipasDB.find((equipa) => equipa.id == req.params.idequipa);
+  console.log(equipa);
+  res.json(equipa);
 });
 
+app.post("/equipas", (request, response) => {
+  console.log(request.body.novaEquipa.nomeEquipa);
+  equipasDB.push({
+    id: 5,
+    equipa: request.body.novaEquipa.nomeEquipa,
+    treinador: {
+      nome: request.body.novaEquipa.nomeTreinador,
+      idade: request.body.novaEquipa.idadeTreinador,
+    },
+    pontos: request.body.novaEquipa.pontos,
+  });
+  response.json({});
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
-
-
+  console.log(`Example app listening on port ${port}`);
+});
