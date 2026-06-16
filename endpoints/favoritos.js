4f66602c470d0fe9093dd000b275df952a26fc6d
Rodrigo USER <rodrigoc@GGHMHNHX7W-PT001740.local>
2026-06-16 15:18:16 +0100

    guardar favoritos no Firestore

--- endpoints/favoritos.js
+++ endpoints/favoritos.js
@@ -1,32 +1,48 @@
-import favoritosDB from "../DB/favoritos.js";
+import { db } from "../firebase.js";
 
-export const getfavoritos = (req, res) => {
+export const getfavoritos = async (req, res) => {
   const userId = req.query.userId;
 
-  const favoritosDoUser = favoritosDB.filter((favorito) => {
-    return favorito.userId === userId;
+  const snapshot = await db
+    .collection("favoritos")
+    .where("userId", "==", userId)
+    .get();
+
+  const favoritosDoUser = snapshot.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+    };
   });
 
   res.json(favoritosDoUser);
 };
 
-export const postfavorito = (request, response) => {
-  const indice = favoritosDB.findIndex((favorito) => {
-    return (
-      favorito.userId === request.body.novoFavorito.userId &&
-      favorito.equipaIdApi === request.body.novoFavorito.equipaIdApi
-    );
-  });
+export const postfavorito = async (request, response) => {
+  const novoFavorito = request.body.novoFavorito;
 
-  if (indice !== -1) {
-    favoritosDB.splice(indice, 1);
+  const snapshot = await db
+    .collection("favoritos")
+    .where("userId", "==", novoFavorito.userId)
+    .where("equipaIdApi", "==", novoFavorito.equipaIdApi)
+    .get();
+
+  if (!snapshot.empty) {
+    await snapshot.docs[0].ref.delete();
   } else {
-    favoritosDB.push(request.body.novoFavorito);
+    await db.collection("favoritos").add(novoFavorito);
   }
 
-  const favoritosDoUser = favoritosDB.filter((favorito) => {
-    return favorito.userId === request.body.novoFavorito.userId;
-  });
+  const favoritosAtualizados = await db
+    .collection("favoritos")
+    .where("userId", "==", novoFavorito.userId)
+    .get();
 
+  const favoritosDoUser = favoritosAtualizados.docs.map((doc) => {
+    return {
+      id: doc.id,
+      ...doc.data(),
+    };
+  });
   response.json(favoritosDoUser);
 };
